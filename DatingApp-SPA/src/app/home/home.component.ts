8a8a14c90a2c63a730795aa9c0fe80f2976b5683
Sundar Urs <sundarkurs@gmail.com>
2020-04-02 21:47:23 +0530

    Register and Home components with parent child com

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
+export class HomeComponent implements OnInit {
+
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) { }
@@ -15,9 +17,14 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
+  registerToggle(){
+    this.registerMode = !this.registerMode;
+  }
+
   getValues(){
     this.http.get('http://localhost:5020/api/values').subscribe( response => {
       this.values = response;
+      console.log(this.values);
     }, error => {
       console.log(error);
     });
