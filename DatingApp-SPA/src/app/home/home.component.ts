7342c070eb469053164d93231bf99424774c7c44
Sundar Urs <sundarkurs@gmail.com>
2020-04-03 15:11:02 +0530

    Register service and functionality added

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -9,25 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
 
   registerMode = false;
-  values: any;
+  titles: string[] = ['Mr', 'Miss', 'Mrs'];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle(){
     this.registerMode = true;
-  }
-
-  getValues(){
-    this.http.get('http://localhost:5020/api/values').subscribe( response => {
-      this.values = response;
-      console.log(this.values);
-    }, error => {
-      console.log(error);
-    });
   }
 
   cancelRegisterMode(registerMode: boolean){
