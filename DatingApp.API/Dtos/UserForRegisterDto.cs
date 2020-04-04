5b6835cea2705daa9857c768c5baf7753c5e6662
Sundar Urs <sundarkurs@gmail.com>
2020-04-04 13:05:12 +0530

    Global error handling

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 8 characters.")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 8 characters.")]
         public string Password { get; set; }
     }
 }
