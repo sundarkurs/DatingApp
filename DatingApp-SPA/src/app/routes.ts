4133233e0b5c2480a01326ae1d0d18b61a929eef
Sundar Urs <sundarkurs@gmail.com>
2020-04-05 15:37:31 +0530

    Guarding multiple paths

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,9 +6,19 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full'},
+    { path: '', component: HomeComponent},
+    
+    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, // Single path guarding
+
+    { // Multiple path guarding using single Guard
+        path: '', 
+        runGuardsAndResolvers: 'always', 
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+
+    { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
