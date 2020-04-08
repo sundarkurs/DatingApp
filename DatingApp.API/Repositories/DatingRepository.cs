d5741863d5caf81dc5b28518081a07fab1ca2cab
Sundar Urs <sundarkurs@gmail.com>
2020-04-08 16:42:11 +0530

    Added Delete and Set main photo functions in both API and Angular

--- DatingApp.API/Repositories/DatingRepository.cs
+++ DatingApp.API/Repositories/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Models;
@@ -43,6 +44,12 @@ namespace DatingApp.API.Repositories
         public async Task<Photo> GetPhoto(int id)
         {
             var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
+            return photo;
+        }
+
+        public async Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            var photo = await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
             return photo;
         }
     }
--- DatingApp.API/Repositories/IDatingRepository.cs
+++ DatingApp.API/Repositories/IDatingRepository.cs
@@ -12,5 +12,6 @@ namespace DatingApp.API.Repositories
          Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
+         Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
