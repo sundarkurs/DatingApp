c929c8efd0002a2306e8c15da2929dc57df7f4b4
Sundar Urs <sundarkurs@gmail.com>
2020-04-09 14:44:10 +0530

    Reactive form - custom validator added

--- DatingApp-SPA/src/app/components/register/register.component.ts
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -21,7 +21,11 @@ export class RegisterComponent implements OnInit {
       username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', Validators.required)
-    });
+    }, this.passwordMatchValidator);
+  }
+
+  passwordMatchValidator(fg: FormGroup){
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : {mismatch: true};
   }
 
   register(){
